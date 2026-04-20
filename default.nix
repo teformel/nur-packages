ef12b21fbb82574e73d55f3df219dd698bc32be7
maorila <maorila@qq.com>
2026-04-20 11:26:41 +0800

    feat: add ww-manager v2.1.10

--- default.nix
+++ default.nix
@@ -19,7 +19,9 @@
   # flakeModules = { }; # flake-parts modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  # 在这里导出你的包
+  ww-manager = pkgs.callPackage ./pkgs/ww-manager { };
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
