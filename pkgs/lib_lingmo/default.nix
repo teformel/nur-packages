7b01f3628515fca803b8982d365f73e3116e5c6c
maorila <maorila@qq.com>
2026-06-22 17:34:29 +0800

    lib_lingmo的hash提交

--- pkgs/lib_lingmo/default.nix
+++ pkgs/lib_lingmo/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     repo = "lib_lingmo";
     rev = "65138ba3f91c08bf88d3a10b9dc17fe75bd91ff4";
     # TODO: 首次构建将报错，请将报错提供的 Hash 填入此处
-    hash = "sha256-4O25RkO4qN3v7Y04M8c+M9P54A/oA/0mH5+k5l5tq3I=";
+    hash = "sha256-zynhuTsjAqj6pIaklOKBOkQqfv8nfYQqntRQEOGHON8=";
   };
 
   postPatch = ''
