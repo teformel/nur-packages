a0f7a1c3636bc7d7c5afe7db79802fe498ad7698
maorila <maorila>
2026-06-22 17:52:25 +0800

    feat(lingmo-desktop): 批量导入 Lingmo 桌面环境的 9 个核心前端组件

---
+++ pkgs/lingmo-dock/default.nix
@@ -0,0 +1,34 @@
+{ stdenv, lib, fetchFromGitHub, cmake, pkg-config, kdePackages, qt6, lingmoui, lingmo-core, lib_lingmo }:
+
+stdenv.mkDerivation rec {
+  pname = "lingmo-dock";
+  version = "main";
+
+  src = fetchFromGitHub {
+    owner = "LingmoOS";
+    repo = "lingmo-dock";
+    rev = "4f0f30a06515bb45c011eca8c98c278e497ffe0f";
+    # TODO: 首次构建将报错，请将报错提供的 Hash 填入此处
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    kdePackages.extra-cmake-modules
+    kdePackages.wrapQtAppsHook
+    qt6.qttools
+  ];
+
+  buildInputs = [
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtsvg
+    qt6.qtwayland
+    kdePackages.kcoreaddons
+    kdePackages.kwindowsystem
+    lingmoui
+    lingmo-core
+    lib_lingmo
+  ];
+}
